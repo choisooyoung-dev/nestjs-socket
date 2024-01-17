6c3ab117033de1702bdfcfcdac7e09c0e69c8445
sooyoung.choi <sooyoung.choi.dev@gmail.com>
2024-01-17 22:27:51 +0900

    Fix - 에러 해결

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,18 +1,21 @@
 const socket = io(); // io function은 알아서 socket.io를 실행하고 있는 서버를 찾을 것이다!
 
-// 방을 만들것!! (socket IO에는 이미 방기능이 있다!)
-
-const welcome = document.getElementById('welcome');
-const form = welcome.querySelector('form');
-const room = document.getElementById('room');
 const myFace = document.getElementById('myFace');
 const muteBtn = document.getElementById('mute');
 const cameraBtn = document.getElementById('camera');
 const camerasSelect = document.getElementById('cameras');
 
+const call = document.getElementById('call');
+
+call.hidden = true;
+
+// stream받기 : stream은 비디오와 오디오가 결합된 것
 let myStream;
-let muted = false;
-let cameraOff = false;
+
+let muted = false; // 처음에는 음성을 받음
+let cameraOff = false; // 처음에는 영상을 받음
+let roomName;
+let myPeerConnection; // 누군가 getMedia함수를 불렀을 때와 똑같이 stream을 공유하기 위한 변수
 
 async function getCameras() {
     try {
@@ -35,6 +38,7 @@ async function getCameras() {
     }
 }
 
+// https://developer.mozilla.org/ko/docs/Web/API/MediaDevices/getUserMedia 사용 : 유저의 유저미디어 string을 받기위함
 async function getMedia(deviceId) {
     const initialConstraints = {
         // initialConstraints는 deviceId가 없을 때 실행
@@ -58,8 +62,6 @@ async function getMedia(deviceId) {
         console.log(e);
     }
 }
-
-getMedia();
 
 function handleMuteClick() {
     myStream.getAudioTracks().forEach((track) => (track.enabled = !track.enabled));
@@ -93,86 +95,72 @@ cameraBtn.addEventListener('click', handleCameraClick);
 // 카메라 변경 확인
 camerasSelect.addEventListener('input', handleCameraChange);
 
-room.hidden = true; // 처음에는 방안에서 할 수 있는 것들 안보이게!
+// Welcome Form (join a room)
 
-let roomName;
+const welcome = document.getElementById('welcome');
+const welcomeForm = welcome.querySelector('form');
 
-function addMessage(message) {
-    const ul = room.querySelector('ul');
-    const li = document.createElement('li');
-    li.innerText = message;
-    ul.appendChild(li);
-}
-
-function handleMessageSubmit(event) {
-    event.preventDefault();
-    const input = room.querySelector('#msg input');
-    const value = input.value;
-    socket.emit('new_message', input.value, roomName, () => {
-        addMessage(`You: ${value}`);
-    }); // 백엔드로 new_message 이벤트를 날림, (input.value이랑 방이름도 같이 보냄!), 마지막 요소는 백엔드에서 시작시킬 수 있는 함수!
-    input.value = '';
-}
-
-function handleNicknameSubmit(event) {
-    event.preventDefault();
-    const input = room.querySelector('#name input');
-    socket.emit('nickname', input.value);
-}
-
-function showRoom() {
-    // 방에 들어가면 방 내용이 보이게
+async function initCall() {
     welcome.hidden = true;
-    room.hidden = false;
-    const h3 = room.querySelector('h3');
-    h3.innerText = `Room ${roomName}`; // 저장된 방 이름을 pug의 요소에 전달해서 띄움!
-    const msgForm = room.querySelector('#msg');
-    const nameForm = room.querySelector('#name');
-    msgForm.addEventListener('submit', handleMessageSubmit);
-    nameForm.addEventListener('submit', handleNicknameSubmit);
+    call.hidden = false;
+    await getMedia();
+    makeConnection();
 }
 
-function handleRoomSubmit(event) {
+async function handleWelcomeSubmit(event) {
     event.preventDefault();
-    const input = form.querySelector('input');
-    // argument 보내기 가능 (socketIO는 Object 전달가능)
-    // 첫 번째는 이벤트명(아무거나 상관없음), 두 번째는 front-end에서 전송하는 object(보내고 싶은 payload), 세 번째는 서버에서 호출하는 function
-    socket.emit(
-        // emit의 마지막 요소가 function이면 가능
-        'enter_room',
-        input.value,
-        showRoom, // 백엔드에서 끝났다는 사실을 알리기 위해 function을 넣고 싶다면 맨 마지막에 넣자!
-    ); // 1. socketIO를 이용하면 모든 것이 메세지일 필요가 없다! / 2. client는 어떠한 이벤트든 모두 emit 가능 / 아무거나 전송할 수 있다(text가 아니어도 되고 여러개 전송 가능!)
-    roomName = input.value; // roomName에 입력한 방 이름 저장
+    const input = welcomeForm.querySelector('input');
+    await initCall();
+    socket.emit('join_room', input.value); // 서버로 input value를 보내는 과정!! initCall 함수도 같이 보내준다!
+    roomName = input.value; // 방에 참가했을 때 나중에 쓸 수 있도록 방 이름을 변수에 저장
     input.value = '';
 }
 
-// 서버는 back-end에서 function을 호출하지만 function은 front-end에서 실행됨!!
+welcomeForm.addEventListener('submit', handleWelcomeSubmit);
 
-form.addEventListener('submit', handleRoomSubmit);
+// Socket Code
 
-socket.on('welcome', (user, newCount) => {
-    const h3 = room.querySelector('h3'); // 지금은 showRoom 함수에서 copy&paste 했지만, title을 새로고침해주는 함수를 만들어줘도 좋다!
-    h3.innerText = `Room ${roomName} (${newCount})`; // 저장된 방 이름을 pug의 요소에 전달해서 띄움!
-    addMessage(`${user} arrived!`);
+socket.on('welcome', async () => {
+    const offer = await myPeerConnection.createOffer(); // 다른 사용자를 초대하기 위한 초대장!! (내가 누구인지를 알려주는 내용이 들어있음!)
+    myPeerConnection.setLocalDescription(offer); // myPeerConnection에 내 초대장의 위치 정보를 연결해 주는 과정 https://developer.mozilla.org/ko/docs/Web/API/RTCPeerConnection/setLocalDescription
+    console.log('sent the offer');
+    socket.emit('offer', offer, roomName);
 });
 
-socket.on('bye', (left, newCount) => {
-    const h3 = room.querySelector('h3');
-    h3.innerText = `Room ${roomName} (${newCount})`; // 저장된 방 이름을 pug의 요소에 전달해서 띄움!
-    addMessage(`${left} left ㅠㅠ`);
+socket.on('offer', async (offer) => {
+    console.log('received the offer');
+    myPeerConnection.setRemoteDescription(offer); // 다른 브라우저의 위치를 myPeerConnection에 연결해 주는 과정
+    const answer = await myPeerConnection.createAnswer();
+    myPeerConnection.setLocalDescription(answer); // 현재 브라우저에서 생성한 answer를 현재 브라우저의 myPeerConnection의 LocalDescription으로 등록!!
+    socket.emit('answer', answer, roomName);
+    console.log('sent the answer');
 });
 
-socket.on('new_message', addMessage); // addMessage만 써도 알아서 msg를 매개변수로 넣는다!
+socket.on('answer', (answer) => {
+    console.log('received the answer');
+    myPeerConnection.setRemoteDescription(answer);
+});
 
-socket.on('room_change', (rooms) => {
-    const roomList = welcome.querySelector('ul'); // home.pug에 만든 ul을 가져와서
-    roomList.innerHTML = ''; // roomList의 HTML을 초기화
+socket.on('ice', (ice) => {
+    console.log('received candidate');
+    myPeerConnection.addIceCandidate(ice);
+});
 
-    rooms.forEach((room) => {
-        // rooms 데이터로 받아온 자료들을 li에 하나씩 뿌려준 후 roomsList에 넣어서 출력시킨다
-        const li = document.createElement('li');
-        li.innerText = room;
-        roomList.append(li);
-    });
-}); // 이 작업은 socket.on("room_change", (msg) => console.log(msg));와 같다!
+// RTC code
+
+function makeConnection() {
+    myPeerConnection = new RTCPeerConnection(); // peerConnection을 각각의 브라우저에 생성 https://developer.mozilla.org/ko/docs/Web/API/RTCPeerConnection 참조
+    myPeerConnection.addEventListener('icecandidate', handleIce);
+    myPeerConnection.addEventListener('addstream', handleAddStream);
+    myStream.getTracks().forEach((track) => myPeerConnection.addTrack(track, myStream)); // 영상과 음성 트랙을 myPeerConnection에 추가해줌 -> Peer-to-Peer 연결!!
+}
+
+function handleIce(data) {
+    console.log('sent candidate');
+    socket.emit('ice', data.candidate, roomName);
+}
+
+// function handleAddStream(data) {
+//     const peersFace = document.getElementById('peersFace');
+//     peersFace.srcObject = data.stream;
+// }
--- src/server.js
+++ src/server.js
@@ -19,74 +19,26 @@ app.get('/*', (req, res) => res.redirect('/'));
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 // app.listen(3000, handleListen); // 3000번 포트와 연결
 
-const httpServer = http.createServer(app); // app은 requestlistener 경로 - express application으로부터 서버 생성
-const wsServer = new Server(httpServer, {
-    cors: {
-        origin: ['https://admin.socket.io'], // 이 URL에서 localhost:3000에 액세스할 것이기 때문에! - 온라인에서 Admin UI를 실제로 테스트할 수 있는 데모 사용을 위한 환경설정!
-        credentials: true,
-    },
-}); // localhost:3000/socket.io/socket.io.js로 연결 가능 (socketIO는 websocket의 부가기능이 아니다!!)
+const httpServer = http.createServer(app);
+const wsServer = new Server(httpServer);
+httpServer.listen(3000, handleListen); // 서버는 ws, http 프로토콜 모두 이해할 수 있게 된다!
 
-// instrument(wsServer, {
-//     auth: false, // 실제 비밀번호를 쓰도록 바꿀 수 있음!
-// });
-
-function publicRooms() {
-    const {
-        sockets: {
-            adapter: { sids, rooms },
-        },
-    } = wsServer; // wsServer에서 sids와 rooms 가져오기
-
-    // public room list 만들기
-    const publicRooms = [];
-    rooms.forEach((_, key) => {
-        if (sids.get(key) === undefined) {
-            publicRooms.push(key);
-        }
-    });
-    return publicRooms;
-}
-
-function countRoom(roomName) {
-    // 방에 사람이 몇명이 있는지 계산하는 함수(set의 size를 이용)
-    return wsServer.sockets.adapter.rooms.get(roomName)?.size; // roomName을 찾을 수도 있지만 못찾을 수도 있기 때문에 ?를 붙여준다
-}
-
-// websocket에 비해 개선점 : 1. 어떤 이벤트든지 전달 가능 2. JS Object를 보낼 수 있음
 wsServer.on('connection', (socket) => {
-    socket['nickname'] = 'Anonymous';
-    socket.onAny((event) => {
-        // 미들웨어같은 존재! 어느 이벤트에서든지 console.log를 할 수 있다!
-        // console.log(wsServer.sockets.adapter); // 어댑터 동작 확인하기
-        console.log(`Socket Event:${event}`);
-    });
-
-    socket.on('enter_room', (roomName, done) => {
-        // console.log(socket.rooms); // 현재 들어가있는 방을 표시 (기본적으로 User와 Server 사이에 private room이 있다!)
+    socket.on('join_room', (roomName) => {
         socket.join(roomName);
-        // console.log(socket.rooms);  // 앞은 id, 뒤는 현재 들어가있는 방
-        done();
-        socket.to(roomName).emit('welcome', socket.nickname, countRoom(roomName)); // welcome 이벤트를 roomName에 있는 모든 사람들에게 emit한 것 (하나의 socket에만 메시지 전달), 들어오면 사람수가 바뀌므로 사람수 count!
-        wsServer.sockets.emit('room_change', publicRooms()); // room_change 이벤트의 payload는 publicRooms 함수의 결과 (우리 서버 안에 있는 모든 방의 array = 서버의 모든 socket)
+        socket.to(roomName).emit('welcome'); // 특정 룸에 이벤트 보내기
     });
 
-    socket.on('disconnecting', () => {
-        // 클라이언트가 서버와 연결이 끊어지기 직전에 마지막 굿바이 메시지를 보낼 수 있다!
-        socket.rooms.forEach((room) => socket.to(room).emit('bye', socket.nickname, countRoom(room) - 1)); // 방안에 있는 모두에게 보내기 위해 forEach 사용!, 나가면 사람수가 바뀌므로 사람수 count!
+    socket.on('offer', (offer, roomName) => {
+        // offer이벤트가 들어오면, roomName에 있는 사람들에게 offer 이벤트를 전송하면서 offer를 전송한다.
+        socket.to(roomName).emit('offer', offer);
     });
 
-    socket.on('disconnect', () => {
-        wsServer.sockets.emit('room_change', publicRooms()); // 클라이언트가 종료메시지를 모두에게 보내고 room이 변경되었다고 모두에게 알림!
+    socket.on('answer', (answer, roomName) => {
+        socket.to(roomName).emit('answer', answer);
     });
 
-    socket.on('new_message', (msg, room, done) => {
-        // 메세지랑 done 함수를 받을 것
-        socket.to(room).emit('new_message', `${socket.nickname}: ${msg}`); // new_message 이벤트를 emit한다! 방금 받은 메시지가 payload가 된다!
-        done(); // done은 프론트엔드에서 코드를 실행할 것!! (백엔드에서 작업 다 끝나고!!)
+    socket.on('ice', (ice, roomName) => {
+        socket.to(roomName).emit('ice', ice);
     });
-
-    socket.on('nickname', (nickname) => (socket['nickname'] = nickname));
 });
-
-httpServer.listen(3000, handleListen);
